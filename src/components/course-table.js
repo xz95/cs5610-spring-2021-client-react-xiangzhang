f8f9738fe3fc1dbb58f3eea7079e56fe83ef4cf5
Xiang Zhang <xz95@uw.edu>
2021-02-21 14:26:17 -0800

    add course-manager

---
+++ src/components/course-table.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class CourseTable extends React.Component {
+  render() {
+    return (
+        <div>
+          <h2>Course Table</h2>
+        </div>
+    );
+  }
+}
