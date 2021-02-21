3b6fd34233d56846b92854ad56ccdbe35b81cc70
Xiang Zhang <xz95@uw.edu>
2021-02-21 13:48:29 -0800

    add components dir; add course-editor.js

---
+++ src/services/course-service.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from "../App";
+
+export default class CourseService extends React.Component {
+  render() {
+    return(
+        <h1>Course Service</h1>
+    )
+  }
+}
+
