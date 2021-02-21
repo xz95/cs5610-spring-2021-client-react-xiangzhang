fccc0ca6e37ebf4092e7c9cbe10205bc6dd5dd82
Xiang Zhang <xz95@uw.edu>
2021-02-21 14:23:34 -0800

    add course-table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import CourseService from "./services/course-service";
+import CourseEditor from "./components/course-editor";
 
 function App() {
   return (
     <div className="container-fluid">
       <CourseService/>
+      <CourseEditor/>
     </div>
   );
 }
